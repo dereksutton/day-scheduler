7aab377c75040999ae87bc86b3559f951a43e17d
Derek Sutton <dereksutton86@gmail.com>
2023-02-28 22:44:07 -0500

    updated html and JS file to resolve minor bugs with time blocks save function, other small logic adjustments

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # day-scheduler
-GT Coding Bootcamp | Module 5 | 3rd-Party API's
+
+## Objective
+Build a simple one-page app to save a work schedule for a typical 9am - 5pm day. Each time block will be displayed in a different color, depending whether it's a past, present, or future event. The application uses the Day.JS third-party API for fetching and displaying the date/time.
+
+## Contents
+1. [Features](#features)
+2. [Technology Used](#technology-used)
+3. [Web Page](#web-page)
+4. [Contribution](#contribution)
+
+## Features
+* The time is dynamically updated on the page.
+* When time block is clicked, user is able to add/save new events.
+* When user saves a new event, the content is saved to localStorage.
+* Each time block will display a certain color according to past, present, and future events.
+* The saved events will persist after page refresh.
+* When "RESET PLANNER" button is clicked, the entire schedule is cleared for a new day.
+
+## Technologies Used
+* HTML5
+* CSS3
+* JavaScript
+* BootStrap 5
+* jQuery
+* Day.js
+
+## Web Page
+![Day Scheduler](./assets/deployed-page.png)
+[Deployed Application]()
+
+## Contribution
+* All starter code provided by Georgia Tech Coding Bootcamp.
+* Planner functionality built by [Derek Sutton](https://github.com/dereksutton)
--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,105 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+$(document).ready(function() {
+
+  var planner = [];
+
+  // loads any events stored in localStorage and adds them to the scheduler
+  var taskLoader = function() {
+    planner = JSON.parse(localStorage.getItem("planner")) || [{ time: "", event: "" }];
+
+    planner = planner.map(function(event) {
+      return event || { time: "", event: ""};
+    });
+
+    planner.forEach(function(event) {
+      addEvent(event.time, event.event);
+    });
+  }
+
+  var addEvent = function(eventTime, eventContent) {
+    var taskNew = $("<p>").addClass("m-2 task-new").text(eventContent);
+
+    $("#hour-" + eventTime).find(".description").append(taskNew); 
+  }
+
+  var saveEvent = function() {
+    localStorage.setItem("planner", JSON.stringify(planner));
+  }
+
+  // event listener to edit event item when clicked
+  $(".time-block").on("click", ".description", function() {
+      var content = $(this).text().trim();
+      var newEntry = $("<textarea>", {
+        class: "col-md-10 form-control",
+        val: content
+      });
+
+      $(this).replaceWith(newEntry);
+      newEntry.trigger("focus");
+  });
+
+  // function that saves user text input
+  $(".saveBtn").on("click", function() {
+      var textEntry = $(this).closest(".time-block").find(".form-control");
+
+  // updates the event text
+      var contents = textEntry.val().trim();
+      var eventTime = $(this).closest(".time-block").attr("id").replace("hour-", "");
+
+      var eventInput = $("<div>", {
+        class: "col-md-10 description",
+        html: $("<p>", {
+          class: "m-2",
+          text: contents
+        })
+      });
+
+      textEntry.replaceWith(eventInput);
+
+  // update the planner array
+      var index = $(this).closest(".time-block").index();
+      var eventObj = { time: eventTime, event: contents };
+
+      planner[index] = eventObj;
+      saveEvent();
+      timeAuditor();
+    });
+
+  // loops through and updates the class of each time block based on the current time.
+    var timeAuditor = function() {
+      var now = dayjs();
+      var currentHour = now.hour();
+
+      for (i = 9; i < 18; i++) {
+        var timeBlockEl = $("#hour-" + i).find(".description");
+        timeBlockEl.removeClass("past present future");
+
+        if (currentHour < i) {
+          timeBlockEl.addClass("future");
+        } else if (currentHour > i) {
+          timeBlockEl.addClass("past");
+        } else {
+          timeBlockEl.addClass("present");
+        }
+      }
+    }
+
+    // reset planner
+    $(".reset-btn").on("click", function() {
+      localStorage.clear();
+      $(".task-new").remove();
+    });
+
+    // displays current date and time in the header - dynamically updates via Day.js API
+  $("#currentDay").text("The current time is: " + dayjs().format("h:mm A on dddd, MMMM D" + "."));
+  setInterval(function() {
+    $("#currentDay").text("The current time is: " + dayjs().format("h:mm A on dddd, MMMM D" + "."));
+    timeAuditor();
+  }, 60000);
+
+  taskLoader();
+  timeAuditor();
+
 });
